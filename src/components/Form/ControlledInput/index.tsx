ffb9a80edd6331a86f7157ef9dda6feb90a9f38c
rapzaDev <rapzadev@gmail.com>
2022-01-24 16:27:55 -0300

    Controlled input created

--- src/components/Form/ControlledInput/index.tsx
+++ src/components/Form/ControlledInput/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Control, Controller } from 'react-hook-form';
+import { TextInputProps } from 'react-native';
+
+import { Input } from '../Input';
 
 import {
     Container,
 } from './styles';
 
-function ControlledInput() {
+interface ControlledInputProps extends TextInputProps {
+    control: Control;
+    name: string;
+}
+
+function ControlledInput({ control, name, ...rest }: ControlledInputProps ) {
     return(
         <Container>
-
+            <Controller 
+                control={control}
+                render={ ({ field: { onChange, value } }) => (
+                    <Input 
+                        onChangeText={onChange}
+                        value={value}
+                        {...rest}
+                    />
+                )}
+                name={name}
+            />
         </Container>
     );
 };
